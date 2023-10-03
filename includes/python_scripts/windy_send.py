bf301a4bd4a1e1895af01b03de1aa535e86b22d3
Matvey <66329687+mavotronik@users.noreply.github.com>
2023-10-03 21:35:41 +0300

    Delete includes directory

--- includes/python_scripts/windy_send.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-token = data.get("token")
-temperature = data.get("temperature")
-humidity = data.get("humidity")
-pressure = data.get("pressure")
-
-resp = requests.get(f"https://stations.windy.com/pws/update/{token}?si=0&temp={temperature}&humidity={humidity}&mbar={pressure}")
-# logger.warning(resp.text)
